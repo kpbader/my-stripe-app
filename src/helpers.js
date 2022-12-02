2c57324fa1772a64ac7466766c40eff3a2614d90
Kyle Patrick Bader <kbader91@gmail.com>
2022-12-02 12:07:53 -0800

    need alternative to redirectToCheckout

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,20 @@
 export const isInCart = (product, cartItems) => {
     return cartItems.find(item => item.id === product.id);
 }
+
+// when app is deployed, this will be the final url we deploy the server to 
+const API = 'http://localhost:8080';
+
+export async function fetchFromAPI(endpoint, opts) {
+    const { method, body } = { method: 'POST', body: null, ...opts };
+
+    const res = await fetch(`${API}/${endpoint}`, { 
+        method, 
+        ...(body && { body: JSON.stringify(body) }), 
+        header: {
+            'Content-Type': 'application/json',
+        }
+    });
+
+    return res.json();
+}
--- src/index.js
+++ src/index.js
@@ -6,13 +6,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import ProductsContextProvider from './context/products-context';
 import CartContextProvider from './context/cart-context';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(process.env.REACT_APP_PUBLISHABLE_KEY);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <ProductsContextProvider>
       <CartContextProvider>
-        <App />
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
       </CartContextProvider>
     </ProductsContextProvider>
   </BrowserRouter>
