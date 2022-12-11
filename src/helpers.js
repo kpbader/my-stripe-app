b0f71a370f66818aee58d4c8ba3c39c069dade95
Kyle Patrick Bader <kbader91@gmail.com>
2022-12-10 20:24:54 -0800

    port updated

--- src/helpers.js
+++ src/helpers.js
@@ -12,17 +12,17 @@ export async function fetchFromAPI(endpoint, opts) {
     const user = auth.currentUser;
     const token = user && (await user.getIdToken());
     const res = await fetch(`${API}/${endpoint}`, {
-        method,
-        ...(body && { body: JSON.stringify(body) }),
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`
-        }
+      method,
+      ...(body && { body: JSON.stringify(body) }),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      },
     });
-
+  
     if (res.status === 200) {
-        return res.json();
+      return res.json();
     } else {
-        throw new Error(res.statusText);
+      throw new Error(res.statusText);
     }
-}
+  }
