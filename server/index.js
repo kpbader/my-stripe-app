638e622e9f0330e82bbb4151cd197d33e8755afa
Kyle Patrick Bader <kbader91@gmail.com>
2022-12-14 10:16:10 -0800

    updating root path in server index.js

--- server/index.js
+++ server/index.js
@@ -19,6 +19,9 @@ app.use(express.json({
 app.use(cors({ origin: true }));
 app.use(decodeJWT);
 
+app.get('/', (req, res) => {
+  res.send('root')
+})
 app.get('/get-payment-methods', validateUser, getCards);
 
 app.post('/create-checkout-session', createCheckoutSession);
