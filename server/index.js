e04d1908a99e54272bd8ddf3aab591f3e853c47a
Kyle Patrick Bader <kbader91@gmail.com>
2022-12-14 12:27:40 -0800

    port

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json({
 app.use(cors({ origin: true }));
 app.use(decodeJWT);
 
-// app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', PORT);
 app.get('/get-payment-methods', validateUser, getCards);
 
 app.post('/create-checkout-session', createCheckoutSession);
