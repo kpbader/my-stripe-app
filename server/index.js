b8aeac74b9425a70601354a8c2802c1a4b08a0c6
Kyle Patrick Bader <kbader91@gmail.com>
2022-12-15 09:41:00 -0800

    edited package.json in frontend folder

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(express.json({
 app.use(cors({ origin: true }));
 app.use(decodeJWT);
 
-app.get('/', PORT);
+app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/get-payment-methods', validateUser, getCards);
 
 app.post('/create-checkout-session', createCheckoutSession);
